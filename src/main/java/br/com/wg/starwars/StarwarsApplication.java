44d089366d8ac574affed906ec636534498f7d06
Wallace Gonçalves <wallacecosme@gmail.com>
2024-01-29 20:39:53 -0300

    bean creation

--- src/main/java/br/com/wg/starwars/StarwarsApplication.java
+++ src/main/java/br/com/wg/starwars/StarwarsApplication.java
@@ -1,13 +1,24 @@
 package br.com.wg.starwars;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class StarwarsApplication {
+
+	@Value("${apistarwars.urlApi}")
+	private String urlApi;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarwarsApplication.class, args);
 	}
 
+	@Bean
+	WebClient clientApi() {
+		return WebClient.create(urlApi);
+	}
 }
